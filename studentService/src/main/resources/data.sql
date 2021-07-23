0cc72f0b47ef454aabf55fe7bc302ebb805c3b0a
Apichat Banyatsupasil <drsarutobi@gmail.com>
2021-07-24 06:00:26 +0700

    add student2

--- studentService/src/main/resources/data.sql
+++ studentService/src/main/resources/data.sql
@@ -1,4 +1,5 @@
 INSERT INTO student(studentId, name, age, gender) VALUES ('st1', 'John',17,'MALE');
+INSERT INTO student(studentId, name, age, gender) VALUES ('st2', 'Jenny',16,'FEMALE');
 INSERT INTO student(studentId, name, age, gender) VALUES ('st3', 'Azam',16,'MALE');
 INSERT INTO student(studentId, name, age, gender) VALUES ('st4', 'Lisa',16,'FEMALE');
 INSERT INTO student(studentId, name, age, gender) VALUES ('st5', 'Shehani',16,'FEMALE');
